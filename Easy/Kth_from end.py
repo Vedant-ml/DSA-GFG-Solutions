ecdc1002e6908ad143837214f3126e0008e65cd5
Vedant-ml <vedantpradhan790@gmail.com>
2026-08-18 20:19:47 +0530

    Add GFG solutions

---
+++ Easy/Kth_from end.py
@@ -0,0 +1,22 @@
+""" Structure of Linked List Node
+class Node:
+    def __init__(self, x):
+        self.data = x
+    ata    self.next = None
+"""
+
+class Solution:
+    def getKthFromLast(self, head, k):
+        fast=head
+        slow=head
+        
+        for i in range(k):
+            if fast is None:
+                return -1
+            fast=fast.next
+        
+        while fast:
+            slow=slow.next
+            fast=fast.next
+        
+        return slow.data
